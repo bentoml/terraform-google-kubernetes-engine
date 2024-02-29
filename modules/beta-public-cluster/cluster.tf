aba59b8d434f3adf845e886186614d50583ab4c8
Sauyon Lee <git@sjle.co>
2024-02-28 20:03:07 -0800

    add reservation affinity

--- modules/beta-public-cluster/cluster.tf
+++ modules/beta-public-cluster/cluster.tf
@@ -815,6 +815,14 @@ resource "google_container_node_pool" "windows_pools" {
         enabled = gcfs_config.value
       }
     }
+    dynamic "reservation_affinity" {
+      for_each = length(lookup(each.value, "reservation_type", "")) > 0 ? [each.value] : []
+      content {
+        consume_reservation_type = lookup(reservation_affinity.value, "reservation_type", "")
+        key                      = lookup(reservation_affinity.value, "reservation_key", "compute.googleapis.com/reservation-name")
+        values                   = split(",", lookup(reservation_affinity.value, "reservation_values", ""))
+      }
+    }
     dynamic "gvnic" {
       for_each = lookup(each.value, "enable_gvnic", false) ? [true] : []
       content {
